0237a4997af801e25b0cc5d2f80e2e63a7bd1c5f
Shubham-raut <123shubhamraut@gmail.com>
2020-12-09 23:22:16 +0530

    added loader

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import City from "../../components/City/City";
-import { Form, Alert } from "react-bootstrap";
+import { Form, Alert, Spinner, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { errorShow, fetchData } from "../../redux/actions";
 
@@ -8,6 +8,7 @@ function Main() {
   const cityData = useSelector((state) => state.cityData);
   const error = useSelector((state) => state.error);
   const showError = useSelector((state) => state.showError);
+  const isFetching = useSelector((state) => state.isFetching);
   const dispatch = useDispatch();
 
   const [cityInput, setCityInput] = useState("");
@@ -37,7 +38,6 @@ function Main() {
   ]);
 
   const citySearchHandlor = (event) => {
-    console.log(cityData);
     event.preventDefault();
     if (cityInput) {
       dispatch(fetchData(cityInput));
@@ -64,10 +64,11 @@ function Main() {
       <h1 className="title">Weather Application</h1>
       <Form onSubmit={citySearchHandlor}>
         <Form.Control
-          placeholder="Search"
+          placeholder="Search for City..."
           value={cityInput}
           onChange={(e) => setCityInput(e.target.value)}
         />
+        <Button variant="secondary">Search</Button>
       </Form>
 
       {showError ? (
@@ -81,13 +82,12 @@ function Main() {
           <Alert.Heading>Opps!</Alert.Heading>
           <p>{error.message}</p>
         </Alert>
+      ) : !isFetching && !cityData ? (
+        <h2 className="title-tag">Search for your City</h2>
       ) : null}
 
-      {cityData ? (
-        <City />
-      ) : (
-        <h2 className="title-tag">Search for your City</h2>
-      )}
+      {isFetching ? <Spinner className="spinner" animation="grow" /> : null}
+      {cityData ? <City /> : null}
     </main>
   );
 }
